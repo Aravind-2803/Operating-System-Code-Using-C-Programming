c3c4d93ab0ef0e75e392f2ab631ae5c24a943451
ARAVIND <aravindv2803@gmail.com>
2025-08-24 10:25:26 +0530

    Create First Come Firstt Serve Problem .c

---
+++ First Come Firstt Serve Problem .c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+	int n,bt[20],wt[20],tat[20],avwt=0,avtat=0,i,j;
+	printf("Enter Total Number of Processes (Maxiimum 20 ) : ");
+	scanf("%d",&n);
+	printf("\nEnter Process Burst Time \n");
+	for(i=0;i<n;i++)
+	{
+		printf("p[%d] : ",i+1);
+		scanf("%d",&bt[i]);
+	}
+	wt[0]=0;
+	for (i=0;i<n;i++)
+	{
+		wt[i]=0;
+		for(j=0;j<i;j++)
+			wt[i] +=bt[j];
+	}
+	printf("\n Process\tBurst Time\tWaiting Time\tTurnaround Time");
+	for(i=0;i<n;i++)
+	{
+		tat[i]=bt[i]+wt[i];
+		avwt += wt[i];
+		avtat += tat[i];
+		printf("\n P[%d]\t\t%d\t\t%d\t\t%d",i+1,bt[i],wt[i],tat[i]);
+	}
+	avwt /=i;
+	avtat /= i;
+	printf("\n\n Average Waiting Time : %d",avwt);
+	printf("\nAverage TurnAround Time : %d \n ",avtat);
+	return 0;
+}
